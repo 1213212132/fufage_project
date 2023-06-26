c9477470d7d5c113cafd3de6f41a2a19b01ef785
LJH <ljf135798@naver.com>
2023-06-26 18:17:47 +0900

    111

--- js/main.js
+++ js/main.js
@@ -73,7 +73,7 @@ $(function () {
     $(".wrap").fullpage({
         navigation: true,//네비게이션 유무
         navigationPosition: "right",//네비게이션 위치
-        sectionsColor: ["#fff", "#fff", "#7baabe", "#666"],//각 섹션 배경색, 배열의 형식
+        sectionsColor: ["#fff", "#4bbfc3", "#7baabe", "#666"],//각 섹션 배경색, 배열의 형식
         navigationTooltips: ["MAIN", "NEWS", "3번", "4번"],//네비게이션 텍스트, 배열의 형식
         showActiveTooltip: true//네비게이션 선택 메뉴 활성화
     });
